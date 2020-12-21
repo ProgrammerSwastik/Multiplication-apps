579a7fa8792cc569d3d5ff183a4f293cf8430d53
Swastik Bhattacharjee <73767874+SwastikBhattacharjee@users.noreply.github.com>
2020-12-21 11:06:10 +0530

    Add files via upload

---
+++ MULTIPLICATION TABLES APP/Untitled1.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+
+int num;
+int i;
+
+int main()
+{
+	
+int num;
+int i;
+
+printf("                                                                                    \n");
+
+printf("*******************************WELCOME TO THE MULTIPLICATION TABLES APP******************************\n");
+	
+printf("                                                                                   \n");
+
+printf("Enter the number of which you want the tables:");
+scanf("%d", & num);
+
+printf("                                                                                    \n");
+	
+printf("%d x 1 = %d\n", num, num*1);
+
+printf("%d x 2 = %d\n", num, num*2);
+
+printf("%d x 3 = %d\n", num, num*3);
+
+printf("%d x 4 = %d\n", num, num*4);
+
+printf("%d x 5 = %d\n", num, num*5);
+
+printf("%d x 6 = %d\n", num, num*6);
+
+printf("%d x 7 = %d\n", num, num*7);
+
+printf("%d x 8 = %d\n", num, num*8);
+
+printf("%d x 9 = %d\n", num, num*9);
+
+printf("%d x 10 = %d\n", num, num*10);
+
+printf("%d x 11 = %d\n", num, num*11);
+
+printf("%d x 12 = %d\n", num, num*12);
+
+printf("%d x 13 = %d\n", num, num*13);
+
+printf("%d x 14 = %d\n", num, num*14);
+
+printf("%d x 15 = %d\n", num, num*15);
+
+printf("%d x 16 = %d\n", num, num*16);
+
+printf("%d x 17 = %d\n", num, num*17);
+
+printf("%d x 18 = %d\n", num, num*18);
+
+printf("%d x 19 = %d\n", num, num*19);
+
+printf("%d x 20 = %d\n", num, num*20);
+
+printf("                                                                                    \n");
+
+printf("So,that's the multiplication tables of %d starting from 1 to 20.\n", num);
+
+printf("                                                                                    \n");
+
+printf("*******************************BYE BYE******************************");
+    
+}
